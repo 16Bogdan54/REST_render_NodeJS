81e7a0c631c4a7ba76ec3b8a37646add81a1145a
Bohdan Yunakov <b.yunakov1654@gmail.com>
2023-04-03 17:49:30 +0300

    model

---
+++ models/user.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+
+const UserSchema = new mongoose.Schema(
+    {
+        name: {type: String, required: true},
+        password: {type: Number, required: true},
+        description: {type: String, required: false}
+    },
+    {
+        versionKey: false
+    }
+)
+
+const UserModel = mongoose.model('User', UserSchema)
+
+module.exports = UserModel
