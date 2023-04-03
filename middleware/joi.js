919f8b1fcbb581efa449713a76fac425e0a66e4d
Bohdan Yunakov <b.yunakov1654@gmail.com>
2023-04-03 18:36:07 +0300

    validations

---
+++ middleware/joi.js
@@ -0,0 +1,37 @@
+const joi = require('joi');
+const logger = require('../logger/logger');
+
+const UserModel = require('../models/userModel')
+
+const Validate = (schema) => {
+    return async (req, res, next) => {
+        try {
+            await schema.validateAsync(req.body);
+
+            next();
+        } catch (err) {
+            logger.error(err)
+
+            return res.status(422).json({err})
+        }
+    }
+}
+
+const Schemas = {
+    user: {
+        create: joi.object({
+            name: joi.string().required(),
+            password: joi.number().required(),
+            description: joi.string()
+        }),
+        update: joi.object({
+            name: joi.string(),
+            password: joi.number(),
+            description: joi.string()
+        })
+    }
+}
+module.exports = {
+    Validate,
+    Schemas
+}
