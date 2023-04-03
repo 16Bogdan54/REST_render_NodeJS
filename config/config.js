43b262827c8e93ec8378be5a294637293e480143
Bohdan Yunakov <b.yunakov1654@gmail.com>
2023-04-03 17:13:01 +0300

    config

--- config/config.js
+++ config/config.js
@@ -1,2 +1,18 @@
 const dotenv = require('dotenv').config();
 
+const MONGO_USERNAME = process.env.MONGO_USERNAME || '';
+const MONGO_PASSWORD = process.env.MONGO_PASSWORD || '';
+const MONGO_URL = `mongodb+srv://${MONGO_USERNAME}:${MONGO_PASSWORD}@1stcluster.scolpvq.mongodb.net/?retryWrites=true&w=majority`
+
+const PORT = process.env.PORT || 3001;
+
+const config = {
+    mongo: {
+        url: MONGO_URL
+    },
+    server: {
+        port: PORT
+    }
+}
+
+module.exports = config;
