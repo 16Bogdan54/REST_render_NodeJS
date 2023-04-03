e0892c0a59d0acf042074df557951132fbecc8ad
Bohdan Yunakov <b.yunakov1654@gmail.com>
2023-04-03 18:53:38 +0300

    routes

--- routes/user.js
+++ routes/user.js
@@ -9,3 +9,4 @@ userRouter.get('/get/:userId', UserController.readById);
 userRouter.get('/get', UserController.readAll);
 userRouter.patch('/update/:userId', Validate(Schemas.user.update), UserController.update);
 userRouter.delete('/delete/:authorId', UserController.deleteById);
+
