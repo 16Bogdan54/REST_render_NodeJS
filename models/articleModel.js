765df015394a474888f9521788bf5e099ae8b99c
Bohdan Yunakov <b.yunakov1654@gmail.com>
2023-04-07 10:38:00 +0300

    article model

--- models/articleModel.js
+++ models/articleModel.js
@@ -1,10 +1,11 @@
 const mongoose = require('mongoose');
+const UserSchema = require('./userModel')
 
 const ArticleSchema = new mongoose.Schema({
     title: {type: String, required: true},
     paragraph: {type: String, required: true},
     date: {type: Date, required: true},
-    author: {type: String, required: true}
+    author: UserSchema
 })
 
 const ArticleModel = mongoose.model('Post', ArticleSchema)
--- models/userModel.js
+++ models/userModel.js
@@ -13,4 +13,7 @@ const UserSchema = new mongoose.Schema(
 
 const UserModel = mongoose.model('User', UserSchema)
 
-module.exports = UserModel
+module.exports = {
+    UserSchema,
+    UserModel
+}
